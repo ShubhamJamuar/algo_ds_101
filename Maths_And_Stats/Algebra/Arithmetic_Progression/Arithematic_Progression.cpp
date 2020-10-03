5238c5b6be3b7cb65e9c67801a3587a36d89d0f1
ShubhamJamuar <55888890+ShubhamJamuar@users.noreply.github.com>
2020-10-03 11:24:33 +0530

    Create Arithematic_Progression.cpp

---
+++ Maths_And_Stats/Algebra/Arithmetic_Progression/Arithematic_Progression.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{  int A,N,D; //Generating Arithematic Progression with first term A, Common difference D, upto N terms
+   A=5;
+   D=8;
+   N=15;
+   for(int i=0;i<N;i++)
+   {  cout<<A+i*D<<" "; }
+   }
+   
